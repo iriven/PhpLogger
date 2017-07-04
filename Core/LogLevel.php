52fac622b1c35fbef7d2b97ac86caaf7fa05bf06
Alfred Tchondjo <iriven@yahoo.fr>
2017-07-04 10:30:23 +0200

    Create LogLevel.php

---
+++ Core/LogLevel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjhc1170
+ * Date: 28/06/2017
+ * Time: 15:00
+ */
+
+namespace Events\Core;
+
+/**
+ * Describes log levels
+ */
+class LogLevel
+{
+    const EMERGENCY = 'emergency';
+    const ALERT     = 'alert';
+    const CRITICAL  = 'critical';
+    const ERROR     = 'error';
+    const WARNING   = 'warning';
+    const NOTICE    = 'notice';
+    const INFO      = 'info';
+    const DEBUG     = 'debug';
+}
